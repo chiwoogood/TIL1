9c95da15ca7f9960c14b7032e5adbc50976fc8ff
chiwoo <chiwooooo@naver.com>
2024-01-03 15:45:43 +0900

    4 days add

--- practice.py
+++ practice.py
@@ -283,3 +283,48 @@
 
 
 #퀵정렬
+# array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+
+# def quick_sort(array, start, end):
+#     if start >= end:
+#         return
+#     pivot = start
+#     left = start + 1
+#     right = end
+
+#     while(left <= right):
+#         while(left <= end and array[left] <= array[pivot]):
+#             left +=1
+        
+#         while(right > start and array[right] >= array[pivot]):
+#             right -=1
+
+#         if(left > right):
+#             array[right], array[pivot] = array[pivot], array[right]
+#         else:
+#             array[left], array[right] = array[right],array[left]
+    
+#     quick_sort(array,start,right -1)
+#     quick_sort(array,right+1,end)
+
+# quick_sort(array,0,len(array)-1)
+# print(array)
+
+
+# 퀵정렬
+
+# lst1 = [1, 5, 2, 4, 7, 8,12,2,0]
+
+# def quick_sort(lst):
+#     if len(lst) <=1:
+#         return lst
+#     pivot = lst[len(lst) // 2]
+#     left = [x for x in lst if x < pivot]
+#     middle = [x for x in lst if x == pivot]
+#     right = [x for x in lst if x > pivot]
+
+#     return quick_sort(left) + middle + quick_sort(right)
+
+# new_lst = quick_sort(lst1)
+# print(new_lst)
+
